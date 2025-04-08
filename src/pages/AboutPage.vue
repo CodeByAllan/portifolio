c389679c35a4a73bd7be370f4d0c7702adbdf16f
codebyallan <allan.pereira.dev@proton.me>
2025-04-08 16:18:36 -0300

    Refactor About page to use dynamic paragraph component and load data from external JSON

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -5,34 +5,36 @@
         <h2 class="text-h4 text-weight-bold">About Me</h2>
         <div class="line"></div>
       </div>
-      <p class="text-body1">
-        I am a web developer passionate about technology, always seeking to learn and build
-        innovative solutions. My core development skills include
-        <strong>HTML, CSS, JavaScript, and TypeScript</strong>, and I have experience with frontend
-        frameworks such as <strong>Vue.js, Quasar Framework, and a little bit of React.js</strong>.
-      </p>
-      <p class="text-body1">
-        On the backend, my main stack is <strong>Node.js with TypeScript</strong>, but I also have
-        experience with <strong>Ruby</strong>, where I developed and published a Telegram bot
-        library in the official RubyGems repository. Additionally, I work with
-        <strong>Go (Golang)</strong>, with my first project being a CHIP-8 emulator for Windows,
-        which uses the Fyne library to render games—although currently, the emulator runs only via
-        the command line.
-      </p>
-      <p class="text-body1">
-        I have worked on freelance projects, including modifying a <strong>React.js</strong> landing
-        page. Currently, I work as an <strong>IT Support Technician</strong>, specializing in
-        technical support.
-      </p>
-      <p class="text-body1">
-        I am driven by challenges and love exploring new technologies, whether in web development,
-        building emulators, or automating tasks. If you'd like to exchange ideas about technology or
-        collaborate on something innovative, feel free to reach out—I'm always open to new
-        opportunities!
-      </p>
+
+      <TextParagraph v-for="(text, index) in paragraphs" :key="index" :content="text" />
     </div>
   </q-page>
 </template>
+
+<script lang="ts" setup>
+import TextParagraph from 'src/components/TextParagraph.vue';
+import { ref, onMounted } from 'vue';
+
+const paragraphs = ref<string[]>([]);
+
+onMounted(async () => {
+  const url = process.env.ABOUT_URL;
+  if (!url) {
+    throw new Error('ABOUT_URL is not defined in the environment.');
+  }
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      paragraphs.value = data;
+    } else {
+      console.error('Failed to load JSON file');
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
+</script>
 
 <style scoped>
 .about-container {
@@ -40,30 +42,27 @@
   padding: 24px;
   text-align: center;
 }
+
 .about-title {
   display: flex;
   align-items: center;
   justify-content: center;
 }
+
 .about-title h2 {
   margin-right: 16px;
   color: var(--q-primary);
 }
+
 .line {
   flex-grow: 1;
   height: 2px;
   width: 150px;
   background-color: var(--q-primary);
 }
-.text-body1 {
-  font-size: 28px;
-  line-height: 35px;
-}
+
 @media (max-width: 600px) {
-  .text-body1 {
-    font-size: 16px;
-    line-height: 30px;
-  }
+
   .about-title h2 {
     font-size: 20px;
   }
